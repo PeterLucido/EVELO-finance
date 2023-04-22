885fa945d11eb28924925981f8abb667386f5a4e
Peter Lucido <petermlucido@gmail.com>
2023-04-21 22:52:59 -0500

    add the comment schema and embed it into the expense schema

--- models/expense.js
+++ models/expense.js
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
+
+const commentSchema = new Schema({
+  content: String,
+  date: Date,
+})
 	
 const expenseSchema = new Schema({
   category: {type: String, required: true},
   amount: {type: Number, required: true},
+  comments: [commentSchema],
 })
 
 const Expense = mongoose.model('Expense', expenseSchema)
