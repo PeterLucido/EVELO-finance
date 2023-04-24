9530410ca0338d4b79db69d3071e633099763e87
Peter Lucido <petermlucido@gmail.com>
2023-04-24 16:39:53 -0500

    add images to the pages to the page

--- routes/expenses.js
+++ routes/expenses.js
@@ -1,18 +1,20 @@
 import { Router } from 'express'
 import * as expensesCtrl from '../controllers/expenses.js'
+import { isLoggedIn } from '../middleware/middleware.js'
+
 
 const router = Router()
 
 // GET /expense/new
-router.get('/', expensesCtrl.index)
-router.get('/new', expensesCtrl.new)
-router.get('/:expenseId', expensesCtrl.show)
-router.get('/:expenseId/edit', expensesCtrl.edit)
-router.post('/create', expensesCtrl.create)
-router.post('/:expenseId/comments', expensesCtrl.createComments)
-router.delete('/:expenseId', expensesCtrl.delete)
-router.delete('/:expenseId/comments/:commentId', expensesCtrl.deleteComments)
-router.put('/:expenseId', expensesCtrl.update)
+router.get('/', isLoggedIn, expensesCtrl.index)
+router.get('/new', isLoggedIn, expensesCtrl.new)
+router.get('/:expenseId', isLoggedIn, expensesCtrl.show)
+router.get('/:expenseId/edit', isLoggedIn, expensesCtrl.edit)
+router.post('/create', isLoggedIn, expensesCtrl.create)
+router.post('/:expenseId/comments', isLoggedIn, expensesCtrl.createComments)
+router.delete('/:expenseId', isLoggedIn, expensesCtrl.delete)
+router.delete('/:expenseId/comments/:commentId', isLoggedIn, expensesCtrl.deleteComments)
+router.put('/:expenseId', isLoggedIn, expensesCtrl.update)
 
 
 export { router }
